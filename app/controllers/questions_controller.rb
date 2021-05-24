da099cfe970d2ffbba7230d89b83289e58cd0cc9
Nirmala Luwang <nirmalaluwang@gmail.com>
2021-05-24 18:07:42 +0530

    Stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = ["Great!", "Silly question, get dressed and go to work!", "I don't care, get dressed and go to work!"]
+
+    if @question == 'I am going to work'
+      @answer = @answer[0]
+    elsif @question.end_with?('?')
+      @answer = @answer[1]
+    else
+      @answer = @answer[2]
+    end
+  end
+
+end
